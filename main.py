e2f957a3ad5cafe6c754d9aab00bfabd02e9e87a
Vic8t <vic8.taillieu@gmail.com>
2021-11-23 15:07:52 -0500

    Implement main script

---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+
+from scripts.k_means import k_means_auto_clusters
+from scripts.fc_means import fc_means_auto_clusters
+from scripts.plot import plot_k_values, plot_max_cluster_size
+from scripts.processing import create_windows, prepare_data
+
+
+def main():
+    algo = sys.argv[1]
+    window_size = int(sys.argv[2])
+    window_shift = int(sys.argv[3])
+
+    if algo == "km":
+        clustering = k_means_auto_clusters
+    elif algo == "fcm":
+        clustering = fc_means_auto_clusters
+    else:
+        raise NotImplementedError(f"The algorithm {sys.argv[1]} is not supported")
+
+    df = prepare_data()
+    dates, windows = create_windows(df, window_size, window_shift)
+
+    k_values = []
+    max_cluster_size = []
+
+    for win in tqdm(windows):
+        _, y, k, _ = clustering(win)
+        k_values.append(k)
+        
+        max_size = 0
+        for c in range(k):
+            size = sum(y == c)
+            if size > max_size:
+                max_size = size
+        max_cluster_size.append(max_size)
+
+    plot_k_values(dates, k_values, algo, window_size, window_shift)
+    plot_max_cluster_size(dates, max_cluster_size, algo, window_size, window_shift)
+
+def help():
+    print("\n Usage: python main.py algo window_size window_shift\
+           \n\t algo : 'km' for k-means of 'fcm' for fc-means\
+           \n\t window_size : 21 for a month, 63 for 3 months\
+           \n\t window_shift : equal to window_size if no superposition, else smaller\
+           \n\n\t ex : python main.py fcm 63 21")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        main()
+    else:
+        help()
