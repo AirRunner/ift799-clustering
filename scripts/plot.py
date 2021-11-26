a6a7917de6abc7af15df74e12ed54cdc9292a7df
AirRunner <luca.vaio@efrei.net>
2021-11-25 21:34:54 -0500

    Update plots name (2)

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,22 +1,30 @@
 from matplotlib import pyplot as plt
 
 
-def plot_max_cluster_size(dates, max_cluster_size, algo, window_size, window_shift):
+def plot_max_cluster_size(dates, max_cluster_size, algo, window_size, window_shift, years=None, max_vars=None):
+    filter = "full"
+    if years is not None and max_vars is not None:
+        filter = f"{years[0]}-{years[1]}_{max_vars}"
+
     plt.figure(figsize=(12,8))
 
     plt.title("Max cluster size in function of time")
     plt.xlabel("Date")
     plt.ylabel("Max cluster size")
     plt.plot(dates, max_cluster_size)
-    plt.savefig(f"images/{algo}_max_{window_size}_{window_shift}.png")
+    plt.savefig(f"images/{algo}_max_{window_size}_{window_shift}_{filter}.png")
     plt.show()
 
-def plot_k_values(dates, k_values, algo, window_size, window_shift):
+def plot_k_values(dates, k_values, algo, window_size, window_shift, years=None, max_vars=None):
+    filter = "full"
+    if years is not None and max_vars is not None:
+        filter = f"{years[0]}-{years[1]}_{max_vars}"
+    
     plt.figure(figsize=(12,8))
 
     plt.title("Number of clusters in function of time")
     plt.xlabel("Date")
     plt.ylabel("k")
     plt.plot(dates, k_values)
-    plt.savefig(f"images/{algo}_k_{window_size}_{window_shift}.png")
+    plt.savefig(f"images/{algo}_k_{window_size}_{window_shift}_{filter}.png")
     plt.show()
